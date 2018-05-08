c1eb79bffa4623e7c843176acbb13c68ff81415e
Alexandru Lemnaru <alexandru@librato.com>
2018-05-08 13:47:44 -0700

    Updated to use the host agent from PackageCloud

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo 'Checking connection to Elastic Search ...'
+while true
+do
+    curl -IGET http://grafana:9200
+    curl_exit_code=$?
+    if [[ "$curl_exit_code" = "0" ]]; then
+       echo "Elastic Search service is ONLINE."
+       break
+    else
+       echo "Elastic Search service is OFFLINE($curl_exit_code)."
+    fi
+    sleep 10
+done 
+
+echo 'Starting snap agent ...'
+pushd /opt/appoptics
+/opt/appoptics/sbin/snapteld --config /opt/appoptics/etc/config.yaml --log-path '' --log-level 1
+popd
+echo 'done.'
